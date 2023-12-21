20eb7a2fd0b3d6085fb143b42e02e01cdfb0b692
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2023-12-22 02:35:13 +0900

    프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.py/다른 사람 풀이 추가

--- 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -3,3 +3,8 @@ def solution(dot):
     if (dot[0] < 0) & (dot[1] > 0) : return 2
     if (dot[0] < 0) & (dot[1] < 0) : return 3
     if (dot[0] > 0) & (dot[1] < 0) : return 4
+
+# 다른 사람의 풀이
+def solution2(dot):
+    quad = [(2,1), (3,4)]
+    return quad[dot[0] < 0][dot[1] > 0]
