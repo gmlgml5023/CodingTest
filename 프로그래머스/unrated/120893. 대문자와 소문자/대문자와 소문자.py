1f3e8462dc5e0eff1ab44edb09667e663562dd62
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2023-12-23 14:30:55 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.15 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.py
@@ -6,3 +6,7 @@ def solution(my_string):
         else:
             answer += s.lower()
     return answer
+
+# 한줄로
+def solution2(my_string):
+    return ''.join([s.lower() if s.isupper() else s.upper() for s in my_string])
