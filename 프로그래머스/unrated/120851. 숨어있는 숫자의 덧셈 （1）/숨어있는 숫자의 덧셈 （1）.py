45db1e07e3fbb1223d09de0868418dcd509b92b7
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2023-12-23 14:08:51 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,3 +1,15 @@
 def solution(my_string):
     num = [str(n) for n in range(1,10)]
     return sum([int(s) for s in my_string if s in num])
+
+# 다른 사람 풀이
+def solution2(my_string):
+    return sum([int(s) for s in my_string if s.isdigit()]) # isnumeric()도 가능
+
+
+def solution3(my_string):
+    answer = 0
+    for s in my_string:
+        try : answer = answer + int(s)
+        except : pass
+    return answer
