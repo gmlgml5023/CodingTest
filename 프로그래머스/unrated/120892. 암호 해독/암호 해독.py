bd2558cc9934b23d8f6c26d098c53d42844abc2b
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2023-12-24 12:34:25 +0900

    [level unrated] Title: 암호 해독, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/unrated/120892. 암호 해독/암호 해독.py
@@ -1,2 +1,6 @@
 def solution(cipher, code):
     return ''.join([c[1] for c in enumerate(cipher) if c[0] % code == code-1])
+
+# 다른 사람 풀이
+def solution2(cipher, code):
+    return cipher[code-1::code]
