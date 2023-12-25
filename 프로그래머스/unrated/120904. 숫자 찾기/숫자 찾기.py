9d8ab91bcf41c340e9569edf84ac24a5dce8f7a7
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2023-12-25 12:52:32 +0900

    [level unrated] Title: 숫자 찾기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.py
@@ -1,4 +1,9 @@
 def solution(num, k):
+    return -1 if str(k) not in str(num) else str(num).index(str(k)) + 1
+
+
+
+def solution2(num, k):
     if str(k) in str(num):
         answer = str(num).index(str(k)) + 1
     else:
