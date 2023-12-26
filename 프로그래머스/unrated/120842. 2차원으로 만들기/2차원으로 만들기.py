04f55dd21deb6303b812450f5c2e217963770862
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2023-12-26 16:11:40 +0900

    [level unrated] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,8 +1,5 @@
 def solution(num_list, n):
     answer = []
     for i in range(0,len(num_list),n):
-        if i == 0:
-            answer.append(num_list[0:n])
-        else:
-            answer.append(num_list[i:i+n])
+        answer.append(num_list[i:i+n])
     return answer
