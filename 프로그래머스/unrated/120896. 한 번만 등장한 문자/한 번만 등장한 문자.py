c616dd88292a459963d64199dfa12c9b239b4c8c
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2023-12-27 23:36:02 +0900

    [level unrated] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,2 +1,6 @@
 def solution(s):
     return ''.join(sorted([st for st in s if s.count(st)==1]))
+
+# 중복 조회를 줄일 수 있는 방법
+def solution2(s):
+    return ''.join(sorted([st for st in s if s.count(st)==1]))
