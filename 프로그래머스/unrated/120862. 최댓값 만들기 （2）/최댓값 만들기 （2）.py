96cfce1bf60221117ec32e2f90c5eb1f60944cb8
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-01-01 15:13:29 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,3 +1,3 @@
 def solution(numbers):
     numbers = sorted(numbers)
-    return max(numbers[0] * numbers[1], numbers[-1]*numbers[-2]) 
+    return max(numbers[0] * numbers[1], numbers[-1] * numbers[-2]) 
