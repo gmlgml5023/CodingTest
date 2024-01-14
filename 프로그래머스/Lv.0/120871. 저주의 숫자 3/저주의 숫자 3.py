51bc1062d50c8a1ae4071b2c674e0adfb066b594
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-01-14 13:54:36 +0900

    [level Lv.0] Title: 저주의 숫자 3, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/Lv.0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -2,3 +2,10 @@ def solution(n):
     num_3x = [num3x for num3x in range(1,201) if (num3x%3!=0)&('3' not in str(num3x))][:100]
     num_10 = [num for num in range(1,101)]
     return num_3x[num_10.index(n)]
+
+def solution2(n):
+    for _ in range(n):
+        answer += 1
+        while answer % 3 == 0 or '3' in str(answer):
+            answer += 1
+    return answer
