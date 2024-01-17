1f30ae713bd11b845f9e1920dcbbb8ee466f13a6
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-01-17 12:43:44 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -4,3 +4,12 @@ def solution(n):
         if len([i for i in range(1, num+1) if num%i==0]) > 2:
             composite.append(num)
     return len(composite)
+
+def solution2(n):
+    answer = 0
+    for i in range(4, n+1):
+        for j in range(2, int(i**0.5)+1):
+            if i % j == 0:
+                answer += 1
+                break
+    return answer
