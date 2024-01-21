dc7f892bcf469fc24d72b211d3b9988bb193f3a6
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-01-21 14:34:45 +0900

    [level 0] Title: 특이한 정렬, Time: 0.00 ms, Memory: 9.9 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,2 +1,6 @@
 def solution(numlist, n):
     return sorted(numlist, key = lambda a : (abs(n-a), n-a))
+
+def solution2(numlist, n):
+    l = sorted([(abs(n-num), -num) for num in numlist])
+    return [-m[1] for m in l]
