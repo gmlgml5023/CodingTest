ce9181d2370f31e57ccc2a2e4d28be946db76618
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-01-27 14:36:18 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,12 +1,9 @@
-def solution(a, b):
+def solution(a,b):
     for i in range(min(a,b), 0, -1):
         if (a%i==0)&(b%i==0):
             a = a//i
             b = b//i
-        while True:
-            if b%2!=0: break
-            b = b//2
-        while True:
-            if b%5: break
-            b = b//5
-    return 1 if b==1 else 2
+    for n in [2,5]:
+        while not b % n:
+            b //= n
+    return 1 if b == 1 else 2
