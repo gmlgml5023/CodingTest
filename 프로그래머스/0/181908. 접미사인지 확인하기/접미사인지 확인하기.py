5884c633cfed7a90554bc0ea2951f6ec127ed329
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-02-13 13:29:08 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,2 +1,5 @@
 def solution(my_string, is_suffix):
+    return 1 if my_string[-len(is_suffix):]==is_suffix else 0
+
+def solution2(my_string, is_suffix):
     return int(is_suffix in [my_string[i:] for i in range(len(my_string))])
