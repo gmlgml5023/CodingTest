191dbb026e4fd54b5f6ae67b847b9bcac0351152
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-02-14 16:13:30 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,3 +1,5 @@
 def solution(num_list):
-    m = [n for n in num_list if n < 0]
-    return -1 if len(m)==0 else num_list.index(m[0])
+    for i in range(len(num_list)):
+        if num_list[i] < 0:
+            return i
+    return -1
