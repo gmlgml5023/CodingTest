c91640c3acc23e7fd3d6bf152005ff3f0fcb64dd
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-02-19 14:38:46 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,6 +1,2 @@
 def solution(todo_list, finished):
-    answer = []
-    for i, v in enumerate(finished):
-        if v != True:
-            answer.append(todo_list[i])
-    return answer
+    return [todo_list[i] for i,v in enumerate(finished) if v!=True]
