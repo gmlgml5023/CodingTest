22434800f2afb814b13c0eb1005f0588d301c160
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-02-23 16:30:52 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -1,5 +1,10 @@
-def solution(arr, intervals):
+def solution2(arr, intervals):
     answer = []
     answer.extend(arr[intervals[0][0]:intervals[0][1]+1])
     answer.extend(arr[intervals[1][0]:intervals[1][1]+1])
     return answer
+
+def solution(arr, intervals):
+    s1, e1 = intervals[0]
+    s2, e2 = intervals[1]
+    return arr[s1:e1+1] + arr[s2:e2+1]
