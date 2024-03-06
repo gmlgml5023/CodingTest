fdc7d6ce3cca5cc6346715dfa32c6575d18f16ca
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-03-06 12:05:22 +0900

    [level 0] Title: l로 만들기, Time: 6.36 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,6 +1,6 @@
 def solution(myString):
-    answer = 'abcdefghijk'
-    for s in myString:
-        if s in answer:
-            myString = myString.replace(s, 'l')
-    return myString
+    ch = 'abcdefghijk'
+    return ''.join(['l' if s in ch else s for s in myString])
+
+def solution2(myString):    
+    return ''.join([s if s > 'l' else 'l' for s in myString])
