46c910bacaff45a4fdea6cb3862d13a91a5aa71a
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-03-07 13:55:41 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.65 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -0,0 +1,9 @@
+def solution(n):
+    lst1 = []
+    for i in range(n):
+        lst2 = []
+        for j in range(n):
+            if i == j: lst2.append(1)
+            else: lst2.append(0)
+        lst1.append(lst2)
+    return lst1
