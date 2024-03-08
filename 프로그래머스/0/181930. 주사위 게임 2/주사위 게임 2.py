7fa362a56f4c85d31ae611385888ccd9848fea55
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-03-08 11:55:02 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -5,3 +5,12 @@ def solution(a, b, c):
         return a+b+c
     else:
         return (a+b+c)*(a**2+b**2+c**2)
+    
+def solution2(a,b,c):
+    check = len(set([a,b,c]))
+    if check == 1:
+        return 3*a*3*(a**2)*3*(a**3)
+    elif check==2:
+        return (a+b+c)*(a**2+b**2+c**2)
+    else:
+        return a+b+c
