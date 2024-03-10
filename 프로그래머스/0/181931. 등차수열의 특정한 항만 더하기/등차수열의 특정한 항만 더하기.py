aeca72c58485813bad23bfc1bb3c52242c4943dc
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-03-10 18:16:59 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,2 +1,8 @@
 def solution(a, d, included):
     return sum([a+(d*i[0]) for i in enumerate(included) if i[1]==True])
+
+def solution2(a, d, included):
+    answer = 0
+    for i in range(len(included)):
+        answer += (a+d*i) * int(included[i])
+    return answer
