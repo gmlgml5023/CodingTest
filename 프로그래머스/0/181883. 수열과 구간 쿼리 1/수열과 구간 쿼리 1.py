6de0c1b8313e9d3d3aa5310fb72e0964bdf148ec
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-03-12 11:38:28 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 12.69 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
@@ -1,5 +1,10 @@
-def solution(arr, queries):
+def solution(arr, queries):    
     for i in range(len(queries)):
         for se in range(queries[i][0], queries[i][1]+1):
             arr[se] += 1
+    return arr
+
+def solution2(arr, queries):
+    for s,e in queries:
+        arr = [a+1 if s<=i<=e else a for i, a in enumerate(arr)]
     return arr
