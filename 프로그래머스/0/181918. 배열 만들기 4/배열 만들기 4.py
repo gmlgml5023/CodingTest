ae6487ee6751323d69fee49d13e2993119a5efaf
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-03-21 10:20:41 +0900

    [level 0] Title: 배열 만들기 4, Time: 12.10 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    i = 0
+    stk = []
+    
+    while i < len(arr):
+        if len(stk) == 0:
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] < arr[i]:
+            stk.append(arr[i])
+            i += 1
+        else:
+            stk = stk[:-1]
+    return stk
