40c2e65cb1a2f2d344667853c5b88485942203b6
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-03-23 09:58:32 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -1,2 +1,2 @@
 def solution(s):
-    return ''.join(sorted([ss for ss in s if ss.islower()], reverse=True)) + ''.join(sorted([ss for ss in s if ss.isupper()], reverse=True))
+    return ''.join(sorted(s, reverse=True))
