5d3105a9511a520e1a30378a7791c7943003c8b7
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-04-01 18:46:40 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,2 +1,9 @@
 def solution(t, p):
+    answer = 0
+    for i in range(len(t)-len(p)+1):
+        if t[i:i+len(p)] <= p:
+            answer += 1
+    return answer
+
+def solution_short(t, p):
     return len([t[i:i+len(p)] for i in range(len(t)-len(p)+1) if t[i:i+len(p)] <= p])
