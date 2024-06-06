95b1f62d7cb8b4d511b094c7dd238ad1b57cbe20
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-06-06 12:31:46 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,9 @@
+import sys
+N, M = map(int, sys.stdin.readline().split())
+arr = list(range(1,N+1))
+
+for _ in range(M):
+    i, j = map(int, sys.stdin.readline().split())
+    arr[i-1], arr[j-1] = arr[j-1], arr[i-1]
+    
+print(*arr)
