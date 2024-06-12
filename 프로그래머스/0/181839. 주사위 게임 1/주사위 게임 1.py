1d01ca1e7c0d8392e24f263fc175de7332392b27
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-06-12 22:25:40 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -1,7 +1,7 @@
 def solution(a, b):
     if ((a%2 == 0)and(b%2 != 0)) or ((a%2 != 0)and(b%2 == 0)):
         answer = 2*(a+b)
-    elif (a%2 != 0)&(b%2 != 0):
+    elif (a%2 != 0) & (b%2 != 0):
         answer = a**2 + b**2
     else:
         answer = abs(a-b)
