cb51228be53b37e0a352556955993b1bd3629778
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-06-18 14:17:01 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,10 +1,10 @@
 def solution(my_string):
     answer = [0 for i in range(52)]
-    
     for s in my_string:
         if s.isupper():
             k = 65
         else:
             k = 71
         answer[ord(s)-k] += 1
+        
     return answer
