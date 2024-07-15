b4b532ea7e19558b7872e100a165c920e231cd94
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-07-15 15:49:44 +0900

    [D2] Title: 패턴 마디의 길이, Time: 110 ms, Memory: 41,940 KB -BaekjoonHub

---
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.py
@@ -0,0 +1,8 @@
+N = int(input())
+
+for test_case in range(1, N+1):
+    s = input()
+    for i in range(1, 11):
+        if s[:i] == s[i:2*i]:
+            print(f'#{test_case} {i}')
+            break
