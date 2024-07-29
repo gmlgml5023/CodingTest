7559fb6550801d03134b36714c96fe4ef90c164e
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-07-29 23:13:54 +0900

    [D2] Title: 4834. [파이썬 S/W 문제해결 기본] 1일차 - 숫자 카드, Time: 120 ms, Memory: 44,252 KB -BaekjoonHub

--- SWEA/D2/16180. 4834． ［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드/4834． ［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드.py
+++ SWEA/D2/16180. 4834． ［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드/4834． ［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드.py
@@ -1,11 +1,15 @@
 T = int(input())
-
 for test_case in range(1, T+1):
     N = int(input())
     cards = input()
-    sorted_cards = sorted(list(set(cards)), reverse=True)
-    card_count = [cards.count(card) for card in sorted_cards]
+    cards_cnt = {}
+    for card in cards:
+        if card in cards_cnt:
+            cards_cnt[card] += 1
+        else:
+            cards_cnt[card] = 1
 
-    top_card = int(sorted_cards[card_count.index(max(card_count))])
-
-    print(f'#{test_case} {top_card} {max(card_count)}')
+    max_num = max(cards_cnt, key = lambda x: (cards_cnt[x], x))
+    max_num_cnt = cards_cnt[max_num]
+    
+    print(f'#{test_case} {max_num} {max_num_cnt}')
