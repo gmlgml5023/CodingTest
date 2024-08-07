fe0729496b9492cdc10aaae9b5efc69a684fbd36
gmlgml5023 <135304794+gmlgml5023@users.noreply.github.com>
2024-08-07 14:53:43 +0900

    [D2] Title: 파스칼의 삼각형, Time: 109 ms, Memory: 45,052 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -1,10 +1,8 @@
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
-    triangle = []
-    for line in range(1, N+1):
-        triangle.append([1]*line)
-
+    triangle = [[1]*line for line in range(1, N+1)]
+    
     for j in range(2, N):
         for k in range(j-1):
             triangle[j][k+1] = triangle[j-1][k] + triangle[j-1][k+1]
