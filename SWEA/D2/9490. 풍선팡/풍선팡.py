fabcad5711185476b3d174d6d4b35757a4ea083b
GaHee <135304794+gmlgml5023@users.noreply.github.com>
2024-08-16 10:34:44 +0900

    [D2] Title: 풍선팡, Time: 216 ms, Memory: 49,800 KB -BaekjoonHub

--- SWEA/D2/9490. 풍선팡/풍선팡.py
+++ SWEA/D2/9490. 풍선팡/풍선팡.py
@@ -17,3 +17,26 @@ for tc in range(1, T+1):
             if max_total < total:
                 max_total = total
     print(f'#{tc} {max_total}')
+    
+# T = int(input())
+# for tc in range(1, T+1):
+#     N, M = map(int, input().split())
+#     balloons = [list(map(int, input().split())) for _ in range(N)]
+# 
+#     max_total = 0
+#     di = [0, 0, -1, 1]
+#     dj = [-1, 1, 0, 0]
+# 
+#     for i in range(N):
+#         for j in range(M):
+#             total = balloons[i][j]
+#             for d in range(4):
+#                 for k in range(1, balloons[i][j]+1):
+#                     ni = i + di[d] * k
+#                     nj = j + dj[d] * k
+#                     if 0 <= ni < N and 0 <= nj < M:
+#                         total += balloons[ni][nj]
+# 
+#             if max_total < total:
+#                 max_total = total
+#     print(f'#{tc} {max_total}')
