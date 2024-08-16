5e9ed727c0cd23ed8477a6280e627e1ca5d2c4ac
GaHee <135304794+gmlgml5023@users.noreply.github.com>
2024-08-16 09:45:11 +0900

    [D2] Title: 풍선팡, Time: 220 ms, Memory: 49,804 KB -BaekjoonHub

--- SWEA/D2/9490. 풍선팡/풍선팡.py
+++ SWEA/D2/9490. 풍선팡/풍선팡.py
@@ -1,21 +1,19 @@
 T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
-    arr = [list(map(int, input().split())) for _ in range(N)]
+    balloons = [list(map(int, input().split())) for _ in range(N)]
 
-    max_bomb = 0
-    for row in range(N):  # 행 순회
-        for col in range(M):  # 열 순회
-            bomb = arr[row][col]  # 꽃가루 개수를 더할 bomb 변수에 가운데 값 할당
-            for i in range(1, bomb+1):
-                if col - i >= 0:  # 왼쪽
-                    bomb += arr[row][col-i]
-                if col + i < M:  # 오른쪽
-                    bomb += arr[row][col+i]
-                if row - i >= 0:  # 위쪽
-                    bomb += arr[row-i][col]
-                if row + i < N:  # 아래쪽
-                    bomb += arr[row+i][col]
-                # 최댓값 갱신
-                if max_bomb < bomb : max_bomb = bomb
-    print(f'#{tc} {max_bomb}')
+    max_total = 0
+    for i in range(N):
+        for j in range(M):
+            total = balloons[i][j]
+            for di, dj in [[0, 1], [0, -1], [-1, 0], [1, 0]]:
+                ni, nj = i, j
+                for _ in range(balloons[i][j]):
+                    ni += di
+                    nj += dj
+                    if 0 <= ni < N and 0 <= nj < M:
+                        total += balloons[ni][nj]
+            if max_total < total:
+                max_total = total
+    print(f'#{tc} {max_total}')
