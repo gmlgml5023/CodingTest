963033621660fe2c25c419b49b36acb900737f1e
GaHee <135304794+gmlgml5023@users.noreply.github.com>
2024-08-16 15:13:54 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 122 ms, Memory: 44,780 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,32 +1,23 @@
-T = int(input())
-for tc in range(1, T+1):
-    N, K = map(int, input().split())
-    puzzle = [list(map(int, input().split())) for _ in range(N)]
-
+def where_word(arr):
     possible = 0
     for i in range(N):
-        # 행 검사
-        row = 0
+        blank = 0
         for j in range(N):
-            if puzzle[i][j] == 1:  # 행 우선 순회하며 1이면 빈칸수 +1
-                row += 1
-            else:                  # 중간에 막힌 경우
-                if row == K:     # 빈칸이 단어의 길이와 같으면
+            if arr[i][j] == 1:
+                blank += 1
+            else:
+                if blank == K:  # 빈칸이 단어의 길이와 같으면
                     possible += 1  # 들어갈 수 있는 곳 +1
-                row = 0          # 0으로 초기화
-        if row == K:             # 단어의 길이와 같으면
-            possible += 1          # 들어갈 수 있는 곳 +1
+                blank = 0  # 0으로 초기화
+        if blank == K:  # 단어의 길이와 같으면
+            possible += 1  # 들어갈 수 있는 곳 +1
+    return possible
 
-        # 열 검사
-        col = 0
-        for j in range(N):
-            if puzzle[j][i] == 1:  # 열 우선 순회하며 1이면 빈칸수 +1
-                col += 1
-            else:                  # 중간에 막힌 경우
-                if col == K:     # 빈칸이 단어의 길이와 같으면
-                    possible += 1  # 들어갈 수 있는 곳 +1
-                col = 0          # 0으로 초기화
-        if col == K:             # 단어의 길이와 같으면
-            possible += 1          # 들어갈 수 있는 곳 +1
 
-    print(f'#{tc} {possible}')
+T = int(input())
+for tc in range(1, T + 1):
+    N, K = map(int, input().split())
+    puzzle = [list(map(int, input().split())) for _ in range(N)]
+    puzzle_t = list(map(list, zip(*puzzle)))
+    ans = where_word(puzzle) + where_word(puzzle_t)
+    print(f'#{tc} {ans}')
