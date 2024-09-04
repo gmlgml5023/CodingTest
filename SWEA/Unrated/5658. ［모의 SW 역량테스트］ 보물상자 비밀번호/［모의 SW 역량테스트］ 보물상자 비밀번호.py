b94d1e0bd88335eb438fa7b8859fe1bfe21ed404
GaHee <135304794+gmlgml5023@users.noreply.github.com>
2024-09-04 14:50:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 119 ms, Memory: 48,456 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.py
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.py
@@ -2,12 +2,12 @@ T = int(input())
 for tc in range(1, T+1):
     N, K = map(int, input().split())
     num = input()
-    len_num = N//4
+    len_num = N//4  # 한토막 길이
     lst = []
-    for _ in range(N):
+    for _ in range(len_num):
         for i in range(0, N, len_num):
-            if num[i:i+len_num] not in lst:
+            if num[i:i+len_num] not in lst:  # 중복 검사
                 lst.append(num[i:i+len_num])
-        num = num[-1] + num[:-1]
-    answer = int(sorted(lst, reverse=True)[K-1], 16)
+        num = num[-1] + num[:-1]  # 뚜껑 돌리기
+    answer = int(sorted(lst, reverse=True)[K-1], 16)  # 16진수를 10진수로 변환
     print(f'#{tc} {answer}')
