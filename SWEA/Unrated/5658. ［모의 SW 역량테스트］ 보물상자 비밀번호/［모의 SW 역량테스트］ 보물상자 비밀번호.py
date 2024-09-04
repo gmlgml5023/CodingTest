234601d7293b9e66d8634139e4fdeb5a8568b602
GaHee <135304794+gmlgml5023@users.noreply.github.com>
2024-09-04 14:36:34 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 155 ms, Memory: 50,284 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.py
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.py
@@ -5,9 +5,9 @@ for tc in range(1, T+1):
     len_num = N//4
     lst = []
     for _ in range(N):
-        num = num[-1] + num[:-1]
         for i in range(0, N, len_num):
             if num[i:i+len_num] not in lst:
                 lst.append(num[i:i+len_num])
+        num = num[-1] + num[:-1]
     answer = int(sorted(lst, reverse=True)[K-1], 16)
     print(f'#{tc} {answer}')
