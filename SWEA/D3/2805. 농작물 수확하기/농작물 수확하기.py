62bb414d64e8a46495db7c6b3d7faeb9c4282b20
GaHee <135304794+gmlgml5023@users.noreply.github.com>
2024-10-03 21:57:49 +0900

    [D3] Title: 농작물 수확하기, Time: 195 ms, Memory: 49,560 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -0,0 +1,13 @@
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [list(map(int,input().strip())) for _ in range(N)]
+    total = 0
+    for i in range(N):
+        if i <= N//2:
+            for j in range(N//2-i, N//2+i+1):
+                total += arr[i][j]
+        else:
+            for j in range(N//2-(N-i-1), N//2+(N-i-1)+1):
+                total += arr[i][j]
+    print(f'#{tc} {total}')
